203afe9ae41f9224298d0e1a09f2d6bb886f506d
Zeynep <88207798+zeynepgokdogan@users.noreply.github.com>
2024-07-22 14:26:49 +0300

    Admin & User Multi Login System using Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+
+Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
+Route::get('/post', [HomeController::class, 'post'])->middleware(['auth' ,'admin']);
